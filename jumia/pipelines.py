59f668f42d566f469716b5c9856c6b3e625c6676
louis <690566511@qq.com>
2018-11-26 21:44:53 +0800

    async

--- jumia/pipelines.py
+++ jumia/pipelines.py
@@ -10,83 +10,110 @@ import pymysql
 import pymysql.cursors
 from twisted.enterprise import adbapi
 from twisted.internet import reactor
+import asyncio
+import aiomysql
 
-class JumiaPipeline(object):
-    def __init__(self):
-        self.conn = pymysql.connect(
-            host=settings.MYSQL_HOST,
-            db=settings.MYSQL_DBNAME,
-            user=settings.MYSQL_USER,
-            passwd=settings.MYSQL_PASSWORD,
-            charset='utf8',
-            use_unicode=False
-        )
-        self.cursor = self.conn.cursor()
+# class JumiaPipeline(object):
+#     def __init__(self):
+#         self.conn = pymysql.connect(
+#             host=settings.MYSQL_HOST,
+#             db=settings.MYSQL_DBNAME,
+#             user=settings.MYSQL_USER,
+#             passwd=settings.MYSQL_PASSWORD,
+#             charset='utf8',
+#             use_unicode=False
+#         )
+#         self.cursor = self.conn.cursor()
+#
+#     def process_item(self, item, spider):
+#         self.insertdata(item)
+#         return item
+#
+#     def insertdata(self, item):
+#         itemstr = '"'
+#         dates = str(time.strftime("%Y%m%d"))
+#         print("我要开始处理商品详情页数据")
+#         print(item)
+#         sql = '''insert into jumia_scrapy (l1,l2,l3,goods_name,review,store,sale,rate,product_url,price,dates) values ({l1},{l2},{l3},{goods_name},{review},{store},{sale},{rate},{product_url},{price},{dates})'''.format( \
+#             l1=itemstr + str(item['l1'][0]) + itemstr, l2=itemstr + str(item['l2'][0]) + itemstr,
+#             l3=itemstr + str(item['l3'][0]) + itemstr,
+#             goods_name=itemstr + str(item['goods_name'][0]).replace("'", "").replace('''"''', "") + itemstr,
+#             review=itemstr + str(item['review']) + itemstr, store=itemstr + str(item['store'][0]) + itemstr,
+#             sale=itemstr + str(item['sale']) + itemstr, \
+#             rate=itemstr + str(item['rate']) + itemstr, product_url=item['product_url'],
+#             price=itemstr + str(item['price'][0]) + itemstr, dates=itemstr + dates + itemstr)
+#         print(type(item['review']))
+#         print(sql)
+#         print(item['product_url'])
+#         self.cursor.execute(sql)
+#         self.conn.commit()
+#
+# class MysqlTwistedPipline(object):
+#     def __init__(self, dbpool):
+#         self.dbpool=dbpool
+#
+#     @classmethod
+#     def from_settings(cls, settings):
+#         #读取settings.py文件中的配置
+#         dbparms =dict(
+#             host = settings['MYSQL_HOST'],
+#             db = settings['MYSQL_DBNAME'],
+#             user = settings['MYSQL_USER'],
+#             passwd = settings['MYSQL_PASSWORD'],
+#             charset = 'utf8',
+#             cursorclass=pymysql.cursors.DictCursor,
+#             use_unicode=True,
+#         )
+#         #创建连接池
+#         dbpool = adbapi.ConnectionPool("pymysql", **dbparms)
+#         return cls(dbpool)
+#
+#     def process_item(self, item, spider):
+#         print("创建连接池")
+#         query = self.dbpool.runInteraction(self.insert_data, item)
+#         query.addCallback(self.handle_error)
+#
+#     def handle_error(self, failure):
+#         print(failure)
+#
+#     def insert_data(self, cursor, item):
+#         print("开始异步插入数据")
+#         dates = int(time.time())
+#         insert_sql="insert into jumia_scrapy_tongbu (l1,l2,l3,goods_name,review,store,sale,rate,product_url,price,dates) values (%s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+#         parameters = (item['l1'][0], item['l2'][0], item['l3'][0],item['goods_name'][0], \
+#                                                                        item['review'][0], item['store'][0], item['sale'], item['rate'], \
+#                                                                        item['product_url'], item['price'][0], dates)
+#         print(insert_sql)
+#         try:
+#             cursor.execute(insert_sql, parameters)
+#             print("异步插入成功")
+#         except:
+#             print("异步插入失败")
 
-    def process_item(self, item, spider):
-        self.insertdata(item)
-        return item
+class AiomysqlPipeline(object):
+    def __init__(self, loop):
+        self.host = settings['MYSQL_HOST']
+        self.port = settings['MYSQL_PORT']
+        self.db = settings['MYSQL_DBNAME']
+        self.user = settings['MYSQL_USER']
+        self.passwd = settings['MYSQL_PASSWORD']
+        self.loop = loop
 
-    def insertdata(self, item):
-        itemstr = '"'
+    async def connect(self):
+        self.conn = await aiomysql.connect(self.host, self.port, self.user, self.passwd,
+                                           self.db, self.loop)
+        return self.conn
+
+    async def close(self):
+        await self.conn.close()
+        return True
+
+    async def process_item(self, item, spider, conn):
         dates = str(time.strftime("%Y%m%d"))
-        print("我要开始处理商品详情页数据")
-        print(item)
-        sql = '''insert into jumia_scrapy (l1,l2,l3,goods_name,review,store,sale,rate,product_url,price,dates) values ({l1},{l2},{l3},{goods_name},{review},{store},{sale},{rate},{product_url},{price},{dates})'''.format( \
-            l1=itemstr + str(item['l1'][0]) + itemstr, l2=itemstr + str(item['l2'][0]) + itemstr,
-            l3=itemstr + str(item['l3'][0]) + itemstr,
-            goods_name=itemstr + str(item['goods_name'][0]).replace("'", "").replace('''"''', "") + itemstr,
-            review=itemstr + str(item['review']) + itemstr, store=itemstr + str(item['store'][0]) + itemstr,
-            sale=itemstr + str(item['sale']) + itemstr, \
-            rate=itemstr + str(item['rate']) + itemstr, product_url=item['product_url'],
-            price=itemstr + str(item['price'][0]) + itemstr, dates=itemstr + dates + itemstr)
-        print(type(item['review']))
-        print(sql)
-        print(item['product_url'])
-        self.cursor.execute(sql)
-        self.conn.commit()
-
-class MysqlTwistedPipline(object):
-    def __init__(self, dbpool):
-        self.dbpool=dbpool
-
-    @classmethod
-    def from_settings(cls, settings):
-        #读取settings.py文件中的配置
-        dbparms =dict(
-            host = settings['MYSQL_HOST'],
-            db = settings['MYSQL_DBNAME'],
-            user = settings['MYSQL_USER'],
-            passwd = settings['MYSQL_PASSWORD'],
-            charset = 'utf8',
-            cursorclass=pymysql.cursors.DictCursor,
-            use_unicode=True,
-        )
-        #创建连接池
-        dbpool = adbapi.ConnectionPool("pymysql", **dbparms)
-        return cls(dbpool)
-
-    def process_item(self, item, spider):
-        print("创建连接池")
-        query = self.dbpool.runInteraction(self.insert_data, item)
-        query.addCallback(self.handle_error)
-
-    def handle_error(self, failure):
-        print(failure)
-
-    def insert_data(self, cursor, item):
-        print("开始异步插入数据")
-        dates = int(time.time())
-        insert_sql="insert into jumia_scrapy_tongbu (l1,l2,l3,goods_name,review,store,sale,rate,product_url,price,dates) values\
-                    (%s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        insert_sql = "insert into jumia_scrapy_tongbu (l1,l2,l3,goods_name,review,store,sale,rate,product_url,price,dates) values (%s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         parameters = (item['l1'][0], item['l2'][0], item['l3'][0],item['goods_name'][0], \
-                                                                       item['review'][0], item['store'][0], item['sale'], item['rate'], \
-                                                                       item['product_url'], item['price'][0], dates)
-        print(insert_sql)
-        try:
-            cursor.execute(insert_sql, parameters)
-            print("异步插入成功")
-        except:
-            MysqlTwistedPipline.from_settings()
-            cursor.execute(insert_sql, parameters)
-            print("异步插入失败")
+                                                                                item['review'][0], item['store'][0], item['sale'], item['rate'], \
+                                                                                item['product_url'], item['price'][0], dates)
+        async with self.conn.cursor() as cur:
+            await cur.excute(insert_sql, parameters)
+            print("执行成功")
--- jumia/settings.py
+++ jumia/settings.py
@@ -27,7 +27,8 @@ NEWSPIDER_MODULE = 'jumia.spiders'
 
 ITEM_PIPELINES = {
 #    'jumia.pipelines.JumiaPipeline': 600,
-    'jumia.pipelines.MysqlTwistedPipline': 500,
+#    'jumia.pipelines.MysqlTwistedPipline': 500,
+     'jumia.pipelines.AiomysqlPipeline': 400,
 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -43,7 +44,7 @@ SCHEDULER_FLUSH_ON_START = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.65
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -62,6 +63,7 @@ DOWNLOAD_DELAY = 0.65
 MYSQL_HOST = '96.45.191.65'
 MYSQL_DBNAME = 'louis'     # 数据库名
 MYSQL_USER = 'root'         # 数据库用户
+MYSQL_PORT = 3306
 MYSQL_PASSWORD = '#Yang5224910$%'   # 数据库密码
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
